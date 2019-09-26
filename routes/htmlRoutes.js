ab2f5fbcbf4dacbffd1138a29c22e5b1cb1f48ad
Boyang Matsapola <boyangbass@gmail.com>
2019-09-25 19:06:43 -0500

    pull request for prework

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,21 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  app.get('/',function(req,res){
+    res.redirect('/home');
+  } )
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/home", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
+      res.render("home", {
         msg: "Welcome!",
         examples: dbExamples
       });
     });
+  });
+  app.get("/giftgig", function(req, res) {
+      res.render("giftgig", {});
+  
   });
 
   // Load example page and pass in an example by id
