dc957885753a5feceb721797dfb88e58daeddf02
Aizadik <aizadaboposhova@gmail.com>
2019-09-29 19:04:32 -0500

    added validation part for login

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
+//import { json } from "sequelize/types";
+
 var loginForm = document.getElementById("login-form");
 console.log(loginForm);
 
@@ -16,23 +18,25 @@ loginForm.addEventListener("submit", function(event) {
     }
     const url = "/api/login"
 
-    // post data to the server
-    $.post(url, data, function(error) {
-        if (error) {
-            console.error(error)
-        } else {
-            console.log("it's login in")
-            window.location = '/'
-        }
-    });
+    // ajax call to post data to the server
+    $.post(url, data, function(res) {
+        console.log("it's login in", res)
+            // fix the route for login --> goes home page
 
+        window.localStorage.setItem("token", res.token)
+        window.location = '/home'
+
+
+    }).fail(function(error) {
+        console.log(error)
+        alert(error.responseJSON.message);
+    })
 });
 
 
-// fix the route for login -->goes home page
-
 // logic if the user exist in db than go Home page
+// user login if exist in DB and password match
 
 // Sign up logic - user needs to fill out all required field to sign up
 
-// Logout function
+//
--- public/js/signup.js
+++ public/js/signup.js
@@ -23,13 +23,12 @@ sumbitForm.addEventListener("submit", function(event) {
     const url = "/api/signup"
 
     // post data to the server
-    $.post(url, data, function(error) {
-        if (error) {
-            console.error(error)
-        } else {
-            console.log("it's signing up")
-            window.location = '/profile'
-        }
-    });
+    $.post(url, data, function() {
 
+        console.log("it's signing up")
+        window.location = '/profile'
+
+    }).fail(function() {
+        alert("server is not responding");
+    })
 });
