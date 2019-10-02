2b62ccebcbfd666e515cd6e2bf51c676f71b3e44
Aizadik <aizadaboposhova@gmail.com>
2019-10-02 18:10:42 -0500

    fixed the cookie issue

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,20 +12,20 @@ var kraken = new Kraken({
     api_secret: krakenAPI
 });
 
-module.exports = function (app) {
+module.exports = function(app) {
     // Get all examples      
-    app.get("/api/gigs/:id", function (req, res) {
+    app.get("/api/gigs/:id", function(req, res) {
         db.Gigs.findOne({
             where: {
                 id: req.params.id
             }
-        }).then(function (post) {
+        }).then(function(post) {
             res.json(post);
         });
     });
 
     // Create a new example
-    app.post("/api/gigs", function (req, res) {
+    app.post("/api/gigs", function(req, res) {
         const gigs = db.Gigs;
         const { title, date, location, money, genre, description, instrument } = req.body;
         if (instrument > 1) {
@@ -33,7 +33,7 @@ module.exports = function (app) {
         } else {
             band = instrument[0];
         }
-        gigs.create({ title, date, location, money, genre, description, instrument: band})
+        gigs.create({ title, date, location, money, genre, description, instrument: band })
             .then(data => {
                 res.redirect('/');
                 console.log(data);
@@ -42,7 +42,7 @@ module.exports = function (app) {
     });
 
     // Create a new user
-    app.post("/api/signup", function (req, res) {
+    app.post("/api/signup", function(req, res) {
         const userData = req.body;
         userData.name = userData.name.trim().toLowerCase();
         userData.email = userData.email.trim().toLowerCase();
@@ -51,20 +51,21 @@ module.exports = function (app) {
         // const { name } = req.body;
         // console.log(userData);
         db.User.findOne({ where: { email: userData.email } })
-            .then(function (userResponce) {
+            .then(function(userResponce) {
                 if (userResponce !== null) {
                     throw new Error("This user already exist!")
                 }
 
                 return db.User.create(userData)
             })
-            .then(function (data) {
+            .then(function(data) {
                 console.log('user', data);
-               
-                res.json(data.dataValues);
+
+                token = jwt.sign({ email: data.dataValues.email }, 'grabbygig');
+                res.cookie('token', token).json(data.dataValues);
 
             })
-            .catch(function (error) {
+            .catch(function(error) {
                 console.log("login error", error)
                 res.status(500).json({
                     message: error.message
@@ -73,7 +74,7 @@ module.exports = function (app) {
     });
 
     // login existing user
-    app.post("/api/login", function (req, res) {
+    app.post("/api/login", function(req, res) {
         const userData = req.body;
         userData.email = userData.email.trim().toLowerCase();
         userData.password = hash(userData.password.trim());
@@ -81,18 +82,18 @@ module.exports = function (app) {
 
         //const token = createToken(userData)
         db.User.findOne({ where: { email: userData.email } })
-            .then(function (userResponce) {
+            .then(function(userResponce) {
                 if (userResponce === null) {
                     throw new Error("user is not found")
                 }
                 console.log("keep on eye", userResponce)
-                // function that compares password  
+                    // function that compares password  
                 comparePassword(userResponce.dataValues.password, userData.password);
                 token = jwt.sign({ email: userResponce.email }, 'grabbygig');
                 res.cookie('token', token).status(204).end();
 
             })
-            .catch(function (error) {
+            .catch(function(error) {
                 console.log("login error", error)
                 res.status(500).json({
                     message: error.message
@@ -104,7 +105,7 @@ module.exports = function (app) {
 
 
     // Create a new example
-    app.post("/api/profile", function (req, res) {
+    app.post("/api/profile", function(req, res) {
         const { image, name, location, instrument, bio, YouTubeLinks, UserId } = req.body;
         if (instrument > 1) {
             let band = instrument.join(', ');
@@ -112,14 +113,14 @@ module.exports = function (app) {
             band = instrument[0];
         }
         console.log(image);
-        db.User.update({ image, name, location, instrument: band, bio, YouTubeLinks},{where: {id: UserId}}).then(function (dbProfile) {
+        db.User.update({ image, name, location, instrument: band, bio, YouTubeLinks }, { where: { id: UserId } }).then(function(dbProfile) {
             res.redirect('/home');
         });
     });
 
     // Delete an example by id
-    app.delete("/api/examples/:id", function (req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+    app.delete("/api/examples/:id", function(req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
             res.json(dbExample);
         });
     });
