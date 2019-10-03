510a4b5819b86b1dfe1934518ae85758bd04ddbc
Aizadik <aizadaboposhova@gmail.com>
2019-10-02 20:34:51 -0500

    fixed logout issue

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,27 @@
 require("dotenv").config();
-const apikey = '##';
-    fs = require('fs');
+const apikey = '';
+fs = require('fs');
 var db = require("../models");
 var jwt = require('jsonwebtoken');
 const sgMail = require('@sendgrid/mail');
 let token;
-sgMail.setApiKey('##');
+sgMail.setApiKey("");
 
-module.exports = function (app) {
+module.exports = function(app) {
     // Get all examples      
-    app.get("/api/gigs/:id", function (req, res) {
+    app.get("/api/gigs/:id", function(req, res) {
         db.Gigs.findOne({
             where: {
                 id: req.params.id
             }
-        }).then(function (post) {
+        }).then(function(post) {
             res.json(post);
         });
     });
 
     // Create a new example
-    app.post("/api/gigs", function (req, res) {
-        const {token} = req.cookies;
+    app.post("/api/gigs", function(req, res) {
+        const { token } = req.cookies;
         var decoded = jwt.verify(token, 'grabbygig');
         const gigs = db.Gigs;
         let band;
@@ -31,28 +31,28 @@ module.exports = function (app) {
         } else {
             band = instrument;
         }
-        db.User.findOne({ where: { email: decoded.email } }).then(user =>{
-            gigs.create({ title, date, location, money, genre, description, instrument: band, UserId: user.get('id')})
+        db.User.findOne({ where: { email: decoded.email } }).then(user => {
+            gigs.create({ title, date, location, money, genre, description, instrument: band, UserId: user.get('id') })
                 .then(data => {
                     res.redirect('/');
                     console.log(data);
-    
+
                 })
 
         })
     });
-    app.post("/api/giggrab", function (req, res) {
-        const {token} = req.cookies;
+    app.post("/api/giggrab", function(req, res) {
+        const { token } = req.cookies;
         let TalentId;
         let TalentName;
         var decoded = jwt.verify(token, 'grabbygig');
         const gigs = db.Gigs;
-        const { UserId} = req.body;
-        db.User.findOne({ where: { email: decoded.email} }).then( talent =>{
-                TalentId= talent.id;
-                TalentName = talent.name;
+        const { UserId } = req.body;
+        db.User.findOne({ where: { email: decoded.email } }).then(talent => {
+            TalentId = talent.id;
+            TalentName = talent.name;
 
-            db.User.findOne({ where: { id: UserId } }).then(user =>{
+            db.User.findOne({ where: { id: UserId } }).then(user => {
                 console.log(user.email);
                 console.log(decoded.email);
                 const msg = {
@@ -60,38 +60,38 @@ module.exports = function (app) {
                     from: decoded.email,
                     subject: `${TalentName} wants the gig!`,
                     text: `Hey, Lemme grab that Gig! see my profile at http://www.giggrab.com/${TalentId}! `
-                  };
-                  sgMail.send(msg);
-                  console.log('message sent');
+                };
+                sgMail.send(msg);
+                console.log('message sent');
                 res.status(204).end();
-    
+
             })
 
         })
     });
 
     // Create a new user
-    app.post("/api/signup", function (req, res) {
+    app.post("/api/signup", function(req, res) {
         const userData = req.body;
         userData.name = userData.name.trim().toLowerCase();
         userData.email = userData.email.trim().toLowerCase();
         // hashing the password
         userData.password = hash(userData.password.trim());
         db.User.findOne({ where: { email: userData.email } })
-            .then(function (userResponce) {
+            .then(function(userResponce) {
                 if (userResponce !== null) {
                     throw new Error("This user already exist!")
                 }
 
                 return db.User.create(userData)
             })
-            .then(function (data) {
+            .then(function(data) {
                 console.log('user', data);
-               
+
                 res.json(data.dataValues);
 
             })
-            .catch(function (error) {
+            .catch(function(error) {
                 console.log("login error", error)
                 res.status(500).json({
                     message: error.message
@@ -100,7 +100,7 @@ module.exports = function (app) {
     });
 
     // login existing user
-    app.post("/api/login", function (req, res) {
+    app.post("/api/login", function(req, res) {
         const userData = req.body;
         userData.email = userData.email.trim().toLowerCase();
         userData.password = hash(userData.password.trim());
@@ -108,18 +108,18 @@ module.exports = function (app) {
 
         //const token = createToken(userData)
         db.User.findOne({ where: { email: userData.email } })
-            .then(function (userResponce) {
+            .then(function(userResponce) {
                 if (userResponce === null) {
                     throw new Error("user is not found")
                 }
                 console.log("keep on eye", userResponce)
-                // function that compares password  
+                    // function that compares password  
                 comparePassword(userResponce.dataValues.password, userData.password);
                 token = jwt.sign({ email: userResponce.email }, 'grabbygig');
                 res.cookie('token', token).status(204).end();
 
             })
-            .catch(function (error) {
+            .catch(function(error) {
                 console.log("login error", error)
                 res.status(500).json({
                     message: error.message
@@ -128,23 +128,23 @@ module.exports = function (app) {
 
     });
 
-    app.post("/api/profile", function (req, res) {
+    app.post("/api/profile", function(req, res) {
         let band;
         const { image, name, location, instrument, bio, YouTubeLinks, UserId } = req.body;
         if (instrument > 1) {
-             band = instrument.join(', ');
+            band = instrument.join(', ');
         } else {
             band = instrument;
         }
         console.log(image);
-        db.User.update({ image, name, location, instrument: band, bio, YouTubeLinks},{where: {id: UserId}}).then(function (dbProfile) {
+        db.User.update({ image, name, location, instrument: band, bio, YouTubeLinks }, { where: { id: UserId } }).then(function(dbProfile) {
             res.redirect('/home');
         });
     });
 
     // Delete an example by id
-    app.delete("/api/examples/:id", function (req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+    app.delete("/api/examples/:id", function(req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
             res.json(dbExample);
         });
     });
