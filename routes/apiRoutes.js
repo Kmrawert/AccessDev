d21507dd45e9ed6c2a37c668c2564082af89f4f9
Boyang Matsapola <boyangbass@gmail.com>
2019-10-02 21:29:07 -0500

    merge

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const apikey = 'AFeiQyudCRNK8T2g46sKFz';
+const apikey = '##';
 var Kraken = require('kraken'),
     fs = require('fs');
 var db = require("../models");
@@ -12,7 +12,7 @@ var kraken = new Kraken({
     api_key: password,
     api_secret: krakenAPI
 });
-sgMail.setApiKey('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');
+sgMail.setApiKey('##');
 
 module.exports = function (app) {
     // Get all examples      
